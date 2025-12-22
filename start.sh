bfcac974440d47e19c00eb1f0ef1ce4e50fd7dbe
ikhan <ilkwonhan86@gmail.com>
2025-12-22 12:44:54 +0900

    c

--- start.sh
+++ start.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 set -e
 
-# SSH 데몬 실행 (root)
-service ssh start
+# Tomcat 로그 권한 설정
+mkdir -p $CATALINA_HOME/logs
+chown -R deploy:deploy $CATALINA_HOME/logs
 
-# Tomcat은 deploy 유저로 실행
+# SSH 데몬 실행
+/usr/sbin/sshd
+
+# Tomcat은 deploy 유저로 포그라운드 실행
 su - deploy -c "$CATALINA_HOME/bin/catalina.sh run"
